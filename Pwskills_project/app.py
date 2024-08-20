634fbf96c87046cd998f111f24d6d2697df12567
bj1616 <jha02458@gmail.com>
2024-08-20 21:23:23 +0530

    new file:   Pwskills_project/__pycache__/flask.cpython-312.pyc 	new file:   Pwskills_project/app.py 	new file:   Pwskills_project/main.pkl 	new file:   Pwskills_project/main_project.ipynb 	new file:   Pwskills_project/model.log 	new file:   Pwskills_project/model.pkl 	new file:   Pwskills_project/train_data.csv
    Changes not staged for commit:
    	modified:   Pwskills_project/model.log

---
+++ Pwskills_project/app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+import sklearn
+import pickle
+import numpy as np
+
+model = pickle.load(open("model.pkl","rb"))
+df = pickle.load(open("main.pkl","rb"))
+
+st.header("Oil Price Predictor(In USD)")
+
+date = st.text_input("Date")
+
+month = st.slider("Month",1,12)
+
+year = st.slider("Year",2000,2050)
+
+st.subheader("Click below to predict tomorrow's oil prices today!")
+
+
+if st.button("Predict Price"):
+    values = np.array([date,month,year])
+    values = values.reshape(1,-1)
+    st.title(model.predict(values))
+
+st.feedback("thumbs")
+
+st.image("https://media.istockphoto.com/id/1330124688/photo/gasoline-fuel-nozzle-and-cash-money-gas-price-tax-ethanol-and-fossil-fuel-concept.jpg?s=612x612&w=0&k=20&c=ylQgTvpQs97yLGzhzCHXvwY5UH4l3xN970-l0AEl-_c=")
